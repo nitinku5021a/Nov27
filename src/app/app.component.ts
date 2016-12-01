b74524aa584074297f7a0432e0110e2a9af31951
Nitin Kumar <nitin.iit@gmail.com>
2016-12-01 19:11:09 +0530

    server added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Contact } from './contact';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   contacts:Contact[];
